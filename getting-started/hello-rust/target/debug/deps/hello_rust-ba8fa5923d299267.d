5ae5943bbf57cb0cec23b83474e8de3478ce22b5
laureanray-bahala <laureanray.bahala@hcl.com>
2022-08-15 23:40:42 +0800

    hello rust, again :)

---
+++ getting-started/hello-rust/target/debug/deps/hello_rust-ba8fa5923d299267.d
@@ -0,0 +1,5 @@
+/Users/laureanray.bahala/Projects/learning-rust/getting-started/hello-rust/target/debug/deps/hello_rust-ba8fa5923d299267.rmeta: src/main.rs
+
+/Users/laureanray.bahala/Projects/learning-rust/getting-started/hello-rust/target/debug/deps/hello_rust-ba8fa5923d299267.d: src/main.rs
+
+src/main.rs:
---
+++ getting-started/hello-rust/target/debug/hello-rust.d
@@ -0,0 +1,1 @@
+/Users/laureanray.bahala/Projects/learning-rust/getting-started/hello-rust/target/debug/hello-rust: /Users/laureanray.bahala/Projects/learning-rust/getting-started/hello-rust/src/main.rs
