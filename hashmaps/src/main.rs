4c0db5577b045851c7624cffe40aa6079a468a81
laureanray <laureanraybahala@gmail.com>
2023-06-22 23:55:54 +0800

    hashmaps

--- hashmaps/src/main.rs
+++ hashmaps/src/main.rs
@@ -1,6 +1,13 @@
 use std::collections::HashMap;
 
 fn main() {
+    // first_examples();
+    // overwriting_a_value();
+
+    if_key_not_present();
+}
+
+fn first_examples() {
     let mut scores = HashMap::new();
     scores.insert("Blue".to_string(), 10);
     scores.insert("Yellow".to_string(), 50);
@@ -25,4 +32,30 @@ fn main() {
     for (key, value) in &scores {
         println!("{key} -> {value}");
     }
+}
+
+
+fn overwriting_a_value() {
+    let mut scores: HashMap<String, u64> = HashMap::new();
+
+    let key = "Key";
+    let other = String::from("x");
+    let x = &key[0..1];
+    // scores.insert(x, 3); -> This errors! Because of explicit 'String' type 
+    scores.insert(other, 3);
+
+    println!("{}", x);
+    println!("{}", key);
+    // println!("{}", other);
+
+    println!("{:?}", scores);
+}
+
+fn if_key_not_present() {
+    let mut scores: HashMap<String, i64> = HashMap::new();
+    
+    scores.insert(String::from("Blue"), 10);
+    scores.entry(String::from("Yellow")).or_insert(50);
+
+    println("{:?}", scores);
 }
