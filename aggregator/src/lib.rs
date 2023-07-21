962afd753fb455111a82fc93ff47e68e9c2e73f1
laureanray <laureanraybahala@gmail.com>
2023-07-21 22:32:21 +0800

    trait shenanigans

--- aggregator/src/lib.rs
+++ aggregator/src/lib.rs
@@ -24,6 +24,18 @@ impl Summary for NewsArticle {
     }
 }
 
+// impl Clone for NewsArticle {
+//     fn clone(&self) -> Self {
+//         return self;
+//     }
+// }
+//
+impl Clone for NewsArticle {
+    fn clone(&self) -> Self {
+        *self
+    }
+}
+
 pub struct Tweet {
     pub username: String,
     pub content: String,
@@ -43,6 +55,39 @@ impl Display for Tweet {
     }
 }
 
+impl PartialEq for Tweet {
+    fn eq(&self, other: &Self) -> bool {
+        if self.content == other.content {
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+impl PartialOrd for Tweet {
+    fn gt(&self, other: &Self) -> bool {
+        return false;
+    }
+
+    fn ge(&self, other: &Self) -> bool {
+        return false;
+    }
+
+    fn le(&self, other: &Self) -> bool {
+        return false;
+    }
+
+    fn lt(&self, other: &Self) -> bool {
+        return false;
+    }
+
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        todo!();
+    }
+
+}
+
 pub fn notify<T: Summary + Display>(item: &T) {
     println!("Breaking news! {}", item.summarize())
 }
@@ -60,7 +105,7 @@ where
 }
 
 
-// This doesn't compile
+// This doesn't compile 
 // fn returns_summarizable(switch: bool) -> impl Summary {
 //     if switch {
 //         Tweet {
@@ -78,6 +123,29 @@ where
 //         }
 //     }
 // }
+
+
+struct Pair<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Pair<T> {
+    fn new(x: T, y: T) -> Self {
+        Self { x, y }
+    }
+}
+
+impl<T: Display + PartialOrd> Pair<T> {
+    fn cmp_display(&self) {
+        if self.x >= self.y {
+            println!("The largest member is x = {}", self.x);
+        } else {
+            println!("The largest member is y = {}", self.y);
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -92,5 +160,36 @@ mod tests {
         };
 
         notify(&tweet);
+    }
+
+    #[test]
+    fn pair() {
+        let tweet1 = Tweet { 
+            content: String::from("Hello World!"),
+            username: String::from("@laureanray"),
+            reply: false,
+            retweet: false,
+        };
+
+        let tweet2 = Tweet { 
+            content: String::from("Hello World!"),
+            username: String::from("@laureanray"),
+            reply: false,
+            retweet: false,
+        };
+
+        let na = NewsArticle {
+            headline: String::from("headline"),
+            location: String::from("mindoro"),
+            author: String::from("lr"),
+            content: String::from("news"),
+        };
+
+        let pair = Pair { 
+            x: tweet1,
+            y: tweet2,
+        };
+
+        pair.cmp_display();
     }
 }
