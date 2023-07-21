bff071cfac5262239f35e2418167e203d689444c
laureanray <laureanraybahala@gmail.com>
2023-07-21 23:10:57 +0800

    so thats lifetimes?

--- lifetime/src/main.rs
+++ lifetime/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+
+    let result = longest(string1.as_str(), string2);
+
+    println!("The longest string is {}", result);
 }
+
+fn longest<'a>(str1: &'a str, str2: &'a str) -> &'a str {
+    if str1.len() > str2.len() {
+        return str1;
+    } else {
+        return str2;
+    }
+}
+
