af43ac2f37dd4fa5cb4e7b1882fc214a0ee12df3
laureanray <laureanraybahala@gmail.com>
2023-07-23 18:08:35 +0800

    closure

--- lifetime/src/main.rs
+++ lifetime/src/main.rs
@@ -1,9 +1,10 @@
 fn main() {
     let string1 = String::from("abcd");
-    let string2 = "xyz";
-
-    let result = longest(string1.as_str(), string2);
-
+    let result;
+    { 
+        let string2 = String::from("xyz");
+        result = longest(string1.as_str(), string2.as_str());
+    }
     println!("The longest string is {}", result);
 }
 
