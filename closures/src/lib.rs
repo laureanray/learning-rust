6fae4870a742392882bc716b068741ed1e2486b1
laureanray <laureanraybahala@gmail.com>
2023-07-26 14:32:18 +0800

    closure stuff

--- closures/src/lib.rs
+++ closures/src/lib.rs
@@ -1,10 +1,16 @@
 fn test_closure() {
     let example_closure = |x| x;
-    let n = example_closure(5);
+    let _n = example_closure(5);
     // This would result in a compuler err
     // since closure types are inferred the first time
     // let s = example_closure(String::from("Hello"));
 }
 
-
-
+//
+// fn capturing_ownership() {
+//     let list = vec![1, 2, 3];
+//     println!("Before defining closure: {:?}", list);
+//
+//     let only_borrows || println!("From closure: {:?}", list);
+//
+// }
--- closures/src/main.rs
+++ closures/src/main.rs
@@ -1,11 +1,7 @@
 #[derive(Debug, PartialEq, Copy, Clone)]
 enum ShirtColor {
     Red,
-    Blue,
-}
-
-struct Inventory {
-    shirts: Vec<ShirtColor>,
+    Blue, } struct Inventory { shirts: Vec<ShirtColor>,
 }
 
 impl Inventory {
@@ -33,23 +29,10 @@ impl Inventory {
 }
 
 fn main() {
-    let store = Inventory {
-        shirts: vec![ShirtColor::Blue, ShirtColor::Red, ShirtColor::Blue],
-    };
+    let mut list = vec![1, 2, 3];
+    println!("Before defining closure: {:?}", list);
+    let mut borrows_mutaby = || list.push(7);
+    borrows_mutaby();
 
-    let user_pref1 = Some(ShirtColor::Red);
-    let giveaway1 = store.giveaway(user_pref1);
-
-    println!(
-        "The user with preference {:?} gets {:?}",
-        user_pref1, giveaway1
-    );
-
-    let user_pref2 = None;
-    let giveaway2 = store.giveaway(user_pref2);
-
-    println!(
-        "The user with preference {:?} gets {:?}",
-        user_pref2, giveaway2
-    );
+    println!("after calling the closure: {:?}", list);
 }
